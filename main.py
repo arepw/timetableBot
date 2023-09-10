f00d01c4265045344a148b02e8415c6563124c8a
Artem Samohin <arepw@vk.com>
2023-09-10 16:16:41 +1000

    New version of selenium standalone (116.0). New commands, one for current schedule and another one for next week schedule.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import telebot
 from scrapper.schedule_scrapper import get_schedule_current
 from redis import Redis
 
-
 bot = telebot.TeleBot(os.getenv('TG_TOKEN'))
 
 redis = Redis(host='redis')
@@ -24,29 +23,46 @@ def set_last_update_time() -> str:
 
 @bot.message_handler(commands=["start"])
 def bot_start(m, res=False):
-    bot.send_message(m.chat.id, f'<b>Привет!</b>\nНапиши /timetable чтобы получить расписание на сегодня!'
-                                f'\nРасписание <i>обновляется каждые {int(int(os.getenv("UPDATE_RATE"))/60)} минут,</i>'
+    bot.send_message(m.chat.id, f'<b>Привет!</b>\nНапиши /this_week чтобы получить расписание на эту неделю!'
+                                f'\nИли /next_week - расписание на следущую неделю)'
+                                f'\nРасписание <i>обновляется каждые {int(int(os.getenv("UPDATE_RATE")) / 60)} минут,</i>'
                                 f' следовательно актуально даже при внезапных изменениях 🤩',
                      parse_mode='HTML'
                      )
 
 
-@bot.message_handler(commands=["timetable"])
-def get_schedule(m):
-    bot.send_photo(m.chat.id, photo=open('schedule.png', 'rb'),
-                   caption=f'Лови расписание!\nИ не вздумай прогуливать 😏\n\n<b>Последнее обновление:\n'
-                           f'{redis.get("schedule_last_update").decode("utf8")}</b>',
-                   parse_mode='HTML'
-                   )
+@bot.message_handler(commands=["this_week"])
+def get_schedule_this(m):
+    try:
+        bot.send_photo(m.chat.id, photo=open('schedule.png', 'rb'),
+                       caption=f'Расписание на эту неделю.\n\n<b>Последнее обновление:\n'
+                               f'{redis.get("schedule_last_update").decode("utf8")}</b>',
+                       parse_mode='HTML'
+                       )
+    except OSError:
+        bot.send_message(m.chat.id, text='Ошибка получения расписания.')
 
 
-#
+@bot.message_handler(commands=["next_week"])
+def get_schedule_next(m):
+    try:
+        bot.send_photo(m.chat.id, photo=open('schedule-next.png', 'rb'),
+                       caption=f'Расписание на следующую неделю.\n\n<b>Последнее обновление:\n'
+                               f'{redis.get("schedule_last_update").decode("utf8")}</b>',
+                       parse_mode='HTML'
+                       )
+    except OSError:
+        bot.send_message(m.chat.id,
+                         text='Ошибка получения расписания. '
+                              'Возможно, в личном кабинете нет расписания на следующую неделю.'
+                         )
+
+
 def get_schedule_on_startup():
     """ If there is no schedule.png on bot startup it will run the scrapper. """
     if not os.path.exists(fr'{os.getcwd()}/schedule.png'):
         print('No schedule.png detected in cwd.\nStarting scrapper, usually it takes ~20 seconds.')
         get_schedule_current()
-
         print('Success!')
 
 
